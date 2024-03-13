38079a380c9261e4e66d76cd0b8176e42dfbd76c
Revan Kurniawan <rvnkrwn@gmail.com>
2024-03-13 08:43:04 +0700

    feat: add layout dashboard

--- layouts/dashboard.vue
+++ layouts/dashboard.vue
@@ -1,14 +1,24 @@
 <template>
   <div>
-    <DashboardHeader >
+    <AppPreloading v-if="isLoadingNow" duration="1" />
+    <DashboardHeader>
       <slot />
     </DashboardHeader>
   </div>
 </template>
 
 <script setup lang="ts">
+import DashboardHeader from '~/components/layouts/DashboardHeader.vue'
 
-import DashboardHeader from "~/components/layouts/DashboardHeader.vue";
+const {isLoading} = useAuth()
+const isLoadingNow = computed(() => {
+  return isLoading().value
+})
+
+const { initAuth } = useAuth()
+onBeforeMount(() => {
+  initAuth()
+})
 </script>
 
 <style scoped>
