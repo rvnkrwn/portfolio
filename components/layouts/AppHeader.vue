e25f3e03e0371412f6327f7aa46be841c0a9b2d2
Revan Kurniawan <rvnkrwn@gmail.com>
2024-03-13 09:14:47 +0700

    feat: add layout components

--- components/layouts/AppHeader.vue
+++ components/layouts/AppHeader.vue
@@ -1,83 +1,114 @@
 <template>
   <div>
-    <div id="highlight" class="hidden md:block fixed z-[99999] h-12 w-12 rounded-full border border-base-content" />
-    <AppPreloading />
+    <div id="highlight" class="invisible md:visible fixed z-[99999] h-12 w-12 rounded-full border border-base-content" />
     <header class="fixed z-[9999] w-full h-fit overflow-hidden">
       <div class="wrapper flex justify-between items-center hover:backdrop-blur p-[1em] md:p-[2em]">
         <NuxtLink to="/">
           <div class="protest-guerrilla-regular font-semibold text-xl uppercase flex items-center gap-1">
-            <h1 class="hover:underline">{{ appName?.split(' ')[0] }}</h1>
+            <h1 class="hover:underline">
+              {{ appName?.split(' ')[0] }}
+            </h1>
             <span>-</span>
             <div class="inner-headings h-[30px] overflow-hidden leading-[30px]">
-            <span>
-              &copy; {{year}} <br/>
-              BE Dev <br/>
-              FE Dev <br/>
-              {{ appName?.split(' ')[1] }} <br/>
-            </span>
+              <span>
+                &copy; {{ year }} <br>
+                BE Dev <br>
+                FE Dev <br>
+                {{ appName?.split(' ')[1] }} <br>
+              </span>
             </div>
           </div>
         </NuxtLink>
         <div class="flex items-center">
           <div id="menu-toggle" class="cursor-pointer toggle-hover">
-            <Icon v-if="isOpen" height="24" icon="line-md:menu-to-close-transition" width="24"/>
-            <Icon v-else height="24" icon="line-md:close-to-menu-transition" width="24"/>
+            <Icon v-if="isOpen" height="24" icon="line-md:menu-to-close-transition" width="24" />
+            <Icon v-else height="24" icon="line-md:close-to-menu-transition" width="24" />
           </div>
           <div class="cursor-pointer toggle-hover" @click="darkTheme = !darkTheme">
-            <Icon v-if="darkTheme" height="24" icon="line-md:moon-alt-to-sunny-outline-loop-transition" width="24"/>
-            <Icon v-else height="24" icon="line-md:sunny-outline-to-moon-alt-loop-transition" width="24"/>
+            <Icon v-if="darkTheme" height="24" icon="line-md:moon-alt-to-sunny-outline-loop-transition" width="24" />
+            <Icon v-else height="24" icon="line-md:sunny-outline-to-moon-alt-loop-transition" width="24" />
           </div>
         </div>
       </div>
       <nav class="wrapper items-center !h-full left-0 relative hidden overflow-hidden  p-[1em] md:p-[2em]">
-          <div class="absolute right-16 top-20">
-            <p class="press-start-2p-regular">{{time}}</p>
-          </div>
-          <ul class="text-5xl -mt-28 protest-guerrilla-regular tracking-wider font-extrabold md:text-7xl md:font-black">
-            <li class="nav-item">
-              <NuxtLink active-class="nav-link-active" class="nav-link opacity-70" to="/">Home
-              </NuxtLink>
-            </li>
-            <li class="nav-item">
-              <NuxtLink active-class="nav-link-active" class="nav-link opacity-70" to="/aboutMe">About Me
-              </NuxtLink>
-            </li>
-            <li class="nav-item">
-              <NuxtLink active-class="nav-link-active" class="nav-link opacity-70" to="/experiences">Experiences
-              </NuxtLink>
-            </li>
-            <li class="nav-item">
-              <NuxtLink active-class="nav-link-active" class="nav-link opacity-70" to="/skills">Skills
-              </NuxtLink>
-            </li>
-            <li class="nav-item">
-              <NuxtLink active-class="nav-link-active" class="nav-link opacity-70" to="/blogs">Blogs
-              </NuxtLink>
-            </li>
-            <li class="nav-item">
-              <NuxtLink active-class="nav-link-active" class="nav-link opacity-70" to="/contacts">Contacts
-              </NuxtLink>
-            </li>
-          </ul>
-          <ul class="absolute bottom-32 right-2 grid gap-2 border-base-content/70 p-1 border-r-2 grid-cols-1">
-            <li><NuxtLink to="https://instagram.com/rvnkrwn" class="nav-link opacity-70"><Icon icon="mdi:instagram" width="36" height="36" /></NuxtLink></li>
-            <li><NuxtLink to="https://github.com/rvnkrwn" class="nav-link opacity-70"><Icon icon="mdi:github" width="36" height="36" /></NuxtLink></li>
-            <li><NuxtLink to="https://www.linkedin.com/in/rvnkrwn/" class="nav-link opacity-70"><Icon icon="mdi:linkedin" width="36" height="36" /></NuxtLink></li>
-            <li><NuxtLink to="https://codepen.io/rvnkrwn" class="nav-link opacity-70"><Icon icon="mdi:codepen" width="36" height="36" /></NuxtLink></li>
-            <li><NuxtLink to="mailto:rvnkrwn@gmail.com" class="nav-link opacity-70"><Icon icon="mdi:email" width="36" height="36" /></NuxtLink></li>
-          </ul>
-          <div class="absolute bottom-32 montserrat font-semibold text-sm opacity-70 ">
-            <p class="press-start-2p-regular">{{appName}}</p>
-            <p>Copyright &copy; {{year}}</p>
-          </div>
-        </nav>
+        <div class="absolute right-16 top-20">
+          <p class="press-start-2p-regular">
+            {{ time }}
+          </p>
+        </div>
+        <ul class="text-5xl -mt-28 protest-guerrilla-regular tracking-wider font-extrabold md:text-7xl md:font-black">
+          <li class="nav-item">
+            <NuxtLink active-class="nav-link-active" class="nav-link opacity-70" to="/">
+              Home
+            </NuxtLink>
+          </li>
+          <li class="nav-item">
+            <NuxtLink active-class="nav-link-active" class="nav-link opacity-70" to="/aboutMe">
+              About Me
+            </NuxtLink>
+          </li>
+          <li class="nav-item">
+            <NuxtLink active-class="nav-link-active" class="nav-link opacity-70" to="/experiences">
+              Experiences
+            </NuxtLink>
+          </li>
+          <li class="nav-item">
+            <NuxtLink active-class="nav-link-active" class="nav-link opacity-70" to="/skills">
+              Skills
+            </NuxtLink>
+          </li>
+          <li class="nav-item">
+            <NuxtLink active-class="nav-link-active" class="nav-link opacity-70" to="/blogs">
+              Blogs
+            </NuxtLink>
+          </li>
+          <li class="nav-item">
+            <NuxtLink active-class="nav-link-active" class="nav-link opacity-70" to="/contacts">
+              Contacts
+            </NuxtLink>
+          </li>
+        </ul>
+        <ul class="absolute bottom-32 right-2 grid gap-2 border-base-content/70 p-1 border-r-2 grid-cols-1">
+          <li>
+            <NuxtLink to="https://instagram.com/rvnkrwn" class="nav-link opacity-70">
+              <Icon icon="mdi:instagram" width="36" height="36" />
+            </NuxtLink>
+          </li>
+          <li>
+            <NuxtLink to="https://github.com/rvnkrwn" class="nav-link opacity-70">
+              <Icon icon="mdi:github" width="36" height="36" />
+            </NuxtLink>
+          </li>
+          <li>
+            <NuxtLink to="https://www.linkedin.com/in/rvnkrwn/" class="nav-link opacity-70">
+              <Icon icon="mdi:linkedin" width="36" height="36" />
+            </NuxtLink>
+          </li>
+          <li>
+            <NuxtLink to="https://codepen.io/rvnkrwn" class="nav-link opacity-70">
+              <Icon icon="mdi:codepen" width="36" height="36" />
+            </NuxtLink>
+          </li>
+          <li>
+            <NuxtLink to="mailto:rvnkrwn@gmail.com" class="nav-link opacity-70">
+              <Icon icon="mdi:email" width="36" height="36" />
+            </NuxtLink>
+          </li>
+        </ul>
+        <div class="absolute bottom-32 montserrat font-semibold text-sm opacity-70 ">
+          <p class="press-start-2p-regular">
+            {{ appName }}
+          </p>
+          <p>Copyright &copy; {{ year }}</p>
+        </div>
+      </nav>
     </header>
   </div>
 </template>
 
 <script lang="ts" setup>
-import {gsap} from "gsap"
-import {Icon} from "@iconify/vue";
+import { gsap } from 'gsap'
+import { Icon } from '@iconify/vue'
 
 // const config = useRuntimeConfig()
 const darkTheme = ref(false)
@@ -86,54 +117,53 @@ const year = new Date().getFullYear()
 const time = ref('')
 
 const appName = computed(() => {
-  return "Revan Kurniawan"
+  return 'Revan Kurniawan'
 })
 
 onMounted(() => {
-  const menuToggle = document.getElementById('menu-toggle');
+  const menuToggle = document.getElementById('menu-toggle')
   menuToggle?.addEventListener('click', menuFunction)
 
   const navLinks = document.querySelectorAll('.nav-link')
   navLinks.forEach((navLink) => {
     navLink.addEventListener('mouseover', () => {
-      let tlH = gsap.timeline()
+      const tlH = gsap.timeline()
       tlH.to(navLink, {
         translateX: 100
       })
     })
     navLink.addEventListener('click', menuFunction)
-
   })
 
-  setInterval(updateTime, 1000);
+  setInterval(updateTime, 1000)
 
-  gsap.set('#highlight', {x: 4, color: "white", duration: 3});
+  gsap.set('#highlight', { display: 'none' })
 
-  document.addEventListener("mousemove", (event) => {
-    // Update the animation properties based on the mouse position
+  document.addEventListener('mousemove', (event) => {
     gsap.to('#highlight', {
+      display: 'block',
       duration: 2,
       x: event.clientX,
       y: event.clientY,
       ease: 'elastic'
-    });
-  });
+    })
+  })
 })
 
 const updateTime = () => {
-  const currentTime = new Date();
-  const hours = currentTime.getHours();
-  let minutes: any = currentTime.getMinutes();
-  let seconds: any = currentTime.getSeconds();
+  const currentTime = new Date()
+  const hours = currentTime.getHours()
+  let minutes: any = currentTime.getMinutes()
+  let seconds: any = currentTime.getSeconds()
 
-  minutes = String((minutes < 10 ? "0" : "") + minutes);
-  seconds = String((seconds < 10 ? "0" : "") + seconds);
+  minutes = String((minutes < 10 ? '0' : '') + minutes)
+  seconds = String((seconds < 10 ? '0' : '') + seconds)
 
-  time.value =  hours + ":" + minutes + ":" + seconds; // Adicionei isso para retornar a hora atual
+  time.value = hours + ':' + minutes + ':' + seconds // Adicionei isso para retornar a hora atual
 }
 
 const menuFunction = () => {
-  let tl = gsap.timeline()
+  const tl = gsap.timeline()
   const header = document.querySelector('header')
   const navItems = document.querySelectorAll('.nav-item')
   const navbar = document.querySelector('nav')
@@ -142,7 +172,7 @@ const menuFunction = () => {
     tl.to(header, {
       height: '100dvh',
       duration: 0.5,
-      delay: .3,
+      delay: 0.3,
       onStart: () => {
         navbar?.classList.replace('hidden', 'flex')
         header?.classList.add('h-screen')
@@ -151,11 +181,11 @@ const menuFunction = () => {
         navItems.forEach((navItem) => {
           tl.fromTo(navItem, {
             opacity: 0,
-            translateY: 50,
+            translateY: 50
           }, {
             duration: 0.1,
             opacity: 1,
-            translateY: 0,
+            translateY: 0
           })
         })
       }
@@ -163,7 +193,7 @@ const menuFunction = () => {
     navAbsoluteContent?.forEach((nac) => {
       tl.fromTo(nac, {
         opacity: 0
-      },{
+      }, {
         duration: 0.1,
         delay: 0.2,
         opacity: 1
@@ -171,7 +201,7 @@ const menuFunction = () => {
     })
   } else {
     navAbsoluteContent?.forEach((nac) => {
-      tl.to(nac, 0.1,{
+      tl.to(nac, 0.1, {
         opacity: 0
       })
     })
--- components/layouts/DashboardHeader.vue
+++ components/layouts/DashboardHeader.vue
@@ -4,8 +4,10 @@
       <div id="wrapper-aside" class="p-[1em] relative h-full overflow-x-hidden">
         <section>
           <div class="px-2 py-2 lg:py-4 flex items-center gap-2">
-            <Icon class="inline-block" height="34" icon="arcticons:famistudio" width="34"/>
-            <h1 class="montserrat font-black text-xl tracking-wide">R-STUDIO</h1>
+            <Icon class="inline-block" height="34" icon="arcticons:famistudio" width="34" />
+            <h1 class="montserrat font-black text-xl tracking-wide">
+              R-STUDIO
+            </h1>
           </div>
         </section>
         <hr class="border-base-content/40 my-3">
@@ -14,27 +16,33 @@
             <ul class="montserrat tracking-tight font-normal grid gap-2 lg:gap-4">
               <li>
                 <NuxtLink active-class="active" class="btn shadow-none flex items-center justify-start gap-2 px-4 py-2 rounded-box" to="/admin">
-                  <Icon class="inline-block" height="20" icon="ri:dashboard-horizontal-line" width="20"/>
+                  <Icon class="inline-block" height="20" icon="ri:dashboard-horizontal-line" width="20" />
                   <span class="text-sm md:text-base">Dashboard</span>
                 </NuxtLink>
               </li>
               <li>
                 <NuxtLink active-class="active" class="btn shadow-none flex items-center justify-start gap-2 px-4 py-2 rounded-box" to="/admin/blog">
-                  <Icon class="inline-block" height="20" icon="ri:blogger-line" width="20"/>
+                  <Icon class="inline-block" height="20" icon="ri:blogger-line" width="20" />
                   <span class="text-sm md:text-base">Blog</span>
                 </NuxtLink>
               </li>
               <li>
-                <NuxtLink active-class="active" class="btn shadow-none flex items-center justify-start gap-2 px-4 py-2 rounded-box"
-                          to="/admin/notifications">
-                  <Icon class="inline-block" height="20" icon="ri:notification-2-line" width="20"/>
+                <NuxtLink
+                  active-class="active"
+                  class="btn shadow-none flex items-center justify-start gap-2 px-4 py-2 rounded-box"
+                  to="/admin/notifications"
+                >
+                  <Icon class="inline-block" height="20" icon="ri:notification-2-line" width="20" />
                   <span class="text-sm md:text-base">Notifications</span>
                 </NuxtLink>
               </li>
               <li>
-                <NuxtLink active-class="active" class="btn shadow-none flex items-center justify-start gap-2 px-4 py-2 rounded-box"
-                          to="/admin/setting">
-                  <Icon class="inline-block" height="20" icon="ri:settings-line" width="20"/>
+                <NuxtLink
+                  active-class="active"
+                  class="btn shadow-none flex items-center justify-start gap-2 px-4 py-2 rounded-box"
+                  to="/admin/setting"
+                >
+                  <Icon class="inline-block" height="20" icon="ri:settings-line" width="20" />
                   <span class="text-sm lg:text-base">Setting</span>
                 </NuxtLink>
               </li>
@@ -42,48 +50,105 @@
           </nav>
         </section>
         <section class="flex flex-col items-center montserrat">
-          <div class="avatar">
-            <div class="w-14 rounded-full">
-              <img src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"  alt=""/>
+          <div v-if="user" class="w-full bottom-4 absolute p-[1em]">
+            <div class="p-2 flex flex-col justify-center items-center gap-4">
+              <div class="avatar">
+                <div class="w-14 rounded-full">
+                  <img src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" alt="">
+                </div>
+              </div>
+              <div class="">
+                <h1 class="font-semibold text-sm">
+                  {{ user['name'] }}
+                </h1>
+                <small class="text-xs text-base-content/60">{{ user['email'] }}</small>
+              </div>
+              <button class="btn shadow-none btn-outline w-full flex items-center justify-center gap-2 px-4 py-2 rounded-box" @click="handleLogout">
+                <Icon icon="ri:logout-box-line" width="20" height="20" />
+                <span class="text-sm lg:text-base">Sign Out</span>
+              </button>
             </div>
           </div>
-          <h1 class="mt-2 font-semibold text-sm">Revan Kurniawan</h1>
-          <small class="text-xs text-base-content/60">rvnkrwn@gmail.com</small>
-          <button class="btn btn-outline btn-sm mt-4">Logout</button>
+          <div v-else class="w-full bottom-4 absolute p-[1em]">
+            <div class="p-2">
+              <NuxtLink
+                active-class="active"
+                class="btn shadow-none btn-outline flex items-center justify-center gap-2 px-4 py-2 rounded-box"
+                to="/admin/login"
+              >
+                <Icon icon="ri:login-box-line" width="20" height="20" />
+                <span class="text-sm lg:text-base">Sign In</span>
+              </NuxtLink>
+            </div>
+          </div>
         </section>
       </div>
     </aside>
-    <header>
+    <header class="p-2 w-full">
       <div class="flex items-center gap-4 justify-between w-full">
         <div class="lg:hidden cursor-pointer" @click="openSidebar">
-          <Icon v-if="isOpen" height="24" icon="line-md:menu-to-close-transition" width="24"/>
-          <Icon v-else height="24" icon="line-md:close-to-menu-transition" width="24"/>
+          <Icon v-if="isOpen" height="24" icon="line-md:menu-to-close-transition" width="24" />
+          <Icon v-else height="24" icon="line-md:close-to-menu-transition" width="24" />
         </div>
         <form action="" class="hidden sm:block flex-1">
           <div class="bg-base-300 w-72 px-4 py-2 rounded-box flex items-center">
-            <input type="search" name="search" id="search" placeholder="Search" class="w-full h-full bg-transparent outline-none montserrat text-sm placeholder:text-base-content/60">
+            <input id="search" type="search" name="search" placeholder="Search" class="w-full h-full bg-transparent outline-none montserrat text-sm placeholder:text-base-content/60">
             <Icon icon="ri:search-2-line" width="20" height="20" />
           </div>
         </form>
         <div class="text-sm text-base-content/60">
-          <p>{{currentDate}}</p>
+          <p>{{ currentDate }}</p>
         </div>
       </div>
     </header>
-    <main><slot /></main>
+    <main class="h-full w-full overflow-hidden">
+      <slot />
+    </main>
   </div>
 </template>
 
 <script lang="ts" setup>
-import {Icon} from "@iconify/vue";
-import {gsap} from "gsap"
+import { Icon } from '@iconify/vue'
+import { gsap } from 'gsap'
+import Swal from 'sweetalert2'
 const darkTheme = ref(false)
 
 const days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
 const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
-const date = new Date();
+const date = new Date()
 const currentDate = `${days[date.getDay()]}, ${date.getDate()}th ${months[date.getMonth()]}`
 const isOpen = ref(false)
+
+const { useAuthUser } = useAuth()
+const user = computed(() => {
+  return useAuthUser().value
+})
+
+const handleLogout = () => {
+  Swal.fire({
+    title: 'Are you sure?',
+    text: 'You want to get out of here',
+    icon: 'warning',
+    showCancelButton: true,
+    confirmButtonColor: '#3085d6',
+    cancelButtonColor: '#d33',
+    confirmButtonText: 'Yes, Sign Out!'
+  }).then((result) => {
+    if (result.isConfirmed) {
+      const { useAuthUser, useAuthToken, isLoggedIn } = useAuth()
+      useAuthUser().value = null
+      useAuthToken().value = null
+      isLoggedIn().value = String(false)
+      useCookie('refresh_token').value = null
+      Swal.fire({
+        title: 'Signed Out!',
+        text: 'You have successfully signed out.',
+        icon: 'success'
+      })
+      navigateTo('/admin/login')
+    }
+  })
+}
 
 const openSidebar = () => {
   const tl = gsap.timeline()
@@ -92,7 +157,7 @@ const openSidebar = () => {
       translateX: '100%'
     }, {
       translateX: '0%',
-      duration: .3,
+      duration: 0.3,
       onStart: () => {
         document.querySelector('aside')?.classList.replace('hidden', 'block')
         isOpen.value = true
@@ -103,14 +168,14 @@ const openSidebar = () => {
       translateX: '0%'
     }, {
       translateX: '110%',
-      duration: .3,
+      duration: 0.3,
       onStart: () => {
         isOpen.value = false
-      } ,
+      },
       onComplete: () => {
         const aside = document.querySelector('aside')
         aside?.classList.replace('block', 'hidden')
-        if (aside) aside.style.transform = 'translateX(0)';
+        if (aside) { aside.style.transform = 'translateX(0)' }
       }
     })
   }
@@ -127,21 +192,21 @@ useHead({
 <style scoped>
 #wrapper-dashboard {
   display: grid;
-  grid-template-columns: 20em auto;
+  grid-template-columns: auto auto;
   grid-template-rows: 4.5em
                       auto;
   grid-template-areas:
                 "header header"
                 "main main";
-  height: 100vh;
-  width: 100%;
-  gap: 20px;
-  @apply bg-base-300
+  height: 100dvh;
+  width: 100dvw;
+  gap: 10px;
+  @apply bg-base-300 overflow-hidden
 }
 
 aside {
   grid-area: sidebar;
-  @apply bg-base-200 h-full w-full max-w-[18em] right-0 top-0 shadow-xl shadow-black overflow-y-auto
+  @apply bg-base-200 h-full w-full max-w-[16em] sm:max-w-[18em] right-0 top-0 shadow-xl shadow-black overflow-y-auto
 }
 
 aside #wrapper-aside {
