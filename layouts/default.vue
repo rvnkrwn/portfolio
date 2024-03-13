ff5d06341864cd4bc0a62c15e6604e808c8e4062
Revan Kurniawan <rvnkrwn@gmail.com>
2024-03-13 08:42:40 +0700

    update & chore

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,9 +1,10 @@
 <template>
   <div>
+    <AppPreloading duration="1" />
     <AppHeader />
-      <slot />
+    <slot />
   </div>
 </template>
 <script setup lang="ts">
-import AppHeader from "~/components/layouts/AppHeader.vue";
+import AppHeader from '~/components/layouts/AppHeader.vue'
 </script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,15 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    devtools: {enabled: true},
+  devtools: { enabled: true },
 
-    css: ['~/assets/css/main.css'],
-    postcss: {
-        plugins: {
-            tailwindcss: {},
-            autoprefixer: {},
-        },
-    },
-    runtimeConfig: {
-        appName: 'Revan Kurniawan'
-    },
+  css: ['~/assets/css/main.css'],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {}
+    }
+  },
+  runtimeConfig: {
+    appName: 'Revan Kurniawan',
+    jwtAccessSecret: process.env.ACCESS_SECRET_TOKEN,
+    jwtRefreshSecret: process.env.REFRESH_SECRET_TOKEN
+
+  }
 })
